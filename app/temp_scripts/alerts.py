e951ca0d714f3b4dac52e1b4d9a7b7e8bbdbe77d
sadfaerie <sadfaerie@gmail.com>
2019-07-30 10:25:36 +0100

    os vars

--- app/temp_scripts/alerts.py
+++ app/temp_scripts/alerts.py
@@ -1,13 +1,11 @@
-
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.alertsmanagement import AlertsManagementClient
-import datetime
+import datetime, os
 
-
-tenant_id = '531ff96d-0ae9-462a-8d2d-bec7c0b42082'
-application_id = 'f2279271-d0a0-45ed-ac1c-5aff1388ff01'   # devops_sandbox
-application_secret = '.RS[03PGSl]*1A1L[gTs4+DTuLrkf3*n'   # devops_sandbox
-subscription_id = 'bf308a5c-0624-4334-8ff8-8dca9fd43783'
+tenant_id = os.environ.get('TENANT')
+application_id = os.environ.get('CLIENT_ID_JENKINS')   
+application_secret = os.environ.get('CLIENT_SECRET_JENKINS') 
+subscription_id = os.environ.get('SUBSCRIPTION_ID')
 
 credentials = ServicePrincipalCredentials(
     client_id = application_id,
@@ -23,18 +21,21 @@ alerts = alerts_client.alerts.get_all()
 
 day_ago = datetime.datetime.now() - datetime.timedelta(days = 1)
 
+print(alerts)
 
-def get_alerts(alerts,severity="Sev0"):
+for alert in alerts:
+    print(alert)
+# def get_alerts(alerts):
 
-    for alert in alerts:
-        print("Alert name: {} \n, Alert State {} \n, Alert Severity: {} \n, Target resource name {} \n, Target resource group: {} \n, Start date {}, \n Resolved Date {} \n".format(
-            alert.name, 
-            alert.properties.essentials.alert_state,
-            alert.properties.essentials.severity, 
-            alert.properties.essentials.target_resource_name, 
-            alert.properties.essentials.target_resource_group,
-            alert.properties.essentials.start_date_time,
-            alert.properties.essentials.monitor_condition_resolved_date_time
-            ))
+#     for alert in alerts:
+#         print("Alert name: {} \n, Alert State {} \n, Alert Severity: {} \n, Target resource name {} \n, Target resource group: {} \n, Start date {}, \n Resolved Date {} \n".format(
+#             alert.name, 
+#             alert.properties.essentials.alert_state,
+#             alert.properties.essentials.severity, 
+#             alert.properties.essentials.target_resource_name, 
+#             alert.properties.essentials.target_resource_group,
+#             alert.properties.essentials.start_date_time,
+#             alert.properties.essentials.monitor_condition_resolved_date_time
+#             ))
 
-get_alerts(alerts)
+# get_alerts(alerts)
